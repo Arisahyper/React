29031d89fb38ab15c538929d0dbfe653ba7c541e
Aqua Minato <sweet4182691@gmail.com>
2022-05-16 06:45:52 +0900

    Automatic code formatting and React versioning to ESLint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,12 @@ const App = () => {
 
   return (
     <div className="text-center">
-        <h1 className="text-red-600">Hello Vite + React!</h1>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
+      <h1 className="text-red-600">Hello Vite + React!</h1>
+      <button type="button" onClick={() => setCount((count) => count + 1)}>
+        count is: {count}
+      </button>
     </div>
   );
-}
+};
 
 export { App };
