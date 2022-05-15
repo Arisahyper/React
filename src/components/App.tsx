77c13884448ef56daf94a0a6fb6254851492a0ca
Aqua Minato <sweet4182691@gmail.com>
2022-05-16 06:40:03 +0900

    styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="">
+    <div className="text-center">
         <h1 className="text-red-600">Hello Vite + React!</h1>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
